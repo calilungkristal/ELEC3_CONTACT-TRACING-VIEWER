f71f72e5276549bd4b16a08682e52c707fd6172a
Kristal Calilung <klcalilung@iskolarngbayan.pup.edu.ph>
2022-06-18 15:04:04 +0800

    This commit is for the functionality of the Open Record button.

--- A3_CONTACT-TRACING-VIEWER/Form1.vb
+++ A3_CONTACT-TRACING-VIEWER/Form1.vb
@@ -1,3 +1,12 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub btnOpenRecord_Click(sender As Object, e As EventArgs) Handles btnOpenRecord.Click
+        Dim ofd As OpenFileDialog = New OpenFileDialog()
+        ofd.Filter = "txt Files (*.txt) | *.txt"
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            richTextBoxResult.Text = File.ReadAllText(ofd.FileName)
+        End If
+    End Sub
 End Class
