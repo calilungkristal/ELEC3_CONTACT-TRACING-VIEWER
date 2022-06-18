d1d5930913bb64af494a6ed6ced58ada2719b043
Kristal Calilung <klcalilung@iskolarngbayan.pup.edu.ph>
2022-06-18 15:30:12 +0800

    This commit is for the functionality of the exit button.

--- A3_CONTACT-TRACING-VIEWER/Form1.vb
+++ A3_CONTACT-TRACING-VIEWER/Form1.vb
@@ -13,4 +13,20 @@ Public Class Form1
     Private Sub btnClearList_Click(sender As Object, e As EventArgs) Handles btnClearList.Click
         richTextBoxResult.Clear()
     End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Try
+            Dim iExit As DialogResult
+            iExit = MessageBox.Show("Confirm if you want to exit", "TRACEme.app", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+
+            If iExit = DialogResult.OK Then
+                Application.[Exit]()
+            ElseIf iExit = DialogResult.Cancel Then
+                Return
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "TRACEme.app", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+    End Sub
 End Class
